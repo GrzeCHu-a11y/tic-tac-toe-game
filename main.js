a8c8551467d53808090bf4ac7621ce88f05d0eb7
GrzeCHu-a11y <ggonczowski@zsp5kozuchow.pl>
2024-01-13 16:23:15 +0100

    added check winner func

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const box = document.querySelectorAll(".box");
 
 const winVariants = [
-    [1,2,3],
-    [3,5,6],
-    [7,8,9],
-    [1,4,7],
-    [2,5,8],
-    [3,6,9],
-    [1,5,9],
-    [3,5,7],
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
 ];
 
 let currentPlayer = "X";
@@ -19,8 +19,20 @@ let round = 1;
 const start = (e) => {
     currentPlayer = currentPlayer == "X" ? "O" : "X";
     e.target.innerHTML = currentPlayer;
+    checkWinner()
 }
 
+const checkWinner = () => {
+    for (let index = 0; index < winVariants.length; index++) {
+        const variant = winVariants[index];
+        const a = box[variant[0]].innerHTML;
+        const b = box[variant[1]].innerHTML;
+        const c = box[variant[2]].innerHTML;
 
+        if (a !== "" && a === b && b === c) {
+            alert(`Player ${a} wins!`);
+        }
+    }
+}
 
 box.forEach((box) => box.addEventListener("click", start));
